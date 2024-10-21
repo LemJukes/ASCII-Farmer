92ea9a022b5b20deb1ca1b2b5f9e81ac8db660a7
LemJukes <jukeLTD@proton.me>
2024-10-21 17:48:45 -0400

    whole bunchabullshit

---
+++ alphabuild01/variable_storage.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# Inventory Currency Amount Value Variables
+var coins: int = 100
+var seeds: int = 1
+var water: int = 5
+var waterCap: int = 20
+var crops: int = 2
+
+# Store Price Variables
+var seedPrice: int = 1
+var waterPrice: int = 1
+var cropPrice: int = 1
+var plotPrice: int = 10
+
+# Store Quantity Variables
+var seedQuantity: int = 1
+var waterQuantity: int = 10
+var cropQuantity: int = 1
+
+func _ready() -> void:
+	pass # Replace with function body.
