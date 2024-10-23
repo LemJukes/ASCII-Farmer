0637162760c27b2e12d8d3858df01fce16c5a381
LemJukes <jukeLTD@proton.me>
2024-10-23 17:10:34 -0400

    DAY Two of major work

--- alpha01/primary.gd
+++ alpha01/primary.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # System Variables
-# Player Vars
+	# Player Name Vars
 @onready var player_name = $StartWindow/Background/StartContentStack/PlayerInput/InputWindow/HBoxContainer/VBoxContainer/MarginContainer2/PlayerNameInput
 @onready var player_name_display = $GameWindow/Background/GameWindowContentStack/GameSystemContentStack/SystemInfo/HBoxContainer/PlayerNameDisplay
 
@@ -13,16 +13,32 @@ var elapsed_time: float = 0.0
 # UI Variables
 # Minimize Button Body Vars
 @onready var inv_body = get_node("GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent")
-@onready var tnu_body = get_node("Primary/GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/ToolsAndUpgrades/Background/VBoxContainer/InvTopBar/MarginContainer/HBoxContainer/TopBarButton/TnUMinButton")
-@onready var str_body = get_node("Primary/GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Store/Background/VBoxContainer/StrTopBar/MarginContainer/HBoxContainer/TopBarButton/StrMinButton")
+@onready var tnu_body = get_node("GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/ToolsAndUpgrades/Background/VBoxContainer/TnUWindowContent")
+@onready var str_body = get_node("GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Store/Background/VBoxContainer/StrWindowContent")
+@onready var fld_body = get_node("GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Field/Background/VBoxContainer/FldWindowContent")
+
+# Player Vars
+# Primary Inventory Values
+@onready var coin_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent/InvItems/VBoxContainer/InvItemValues/VBoxContainer/PanelContainer/CoinValLabel
+@onready var seed_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent/InvItems/VBoxContainer/InvItemValues/VBoxContainer2/PanelContainer/SeedValLabel
+@onready var water_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent/InvItems/VBoxContainer/InvItemValues/VBoxContainer3/PanelContainer/HBoxContainer/WaterValLabel
+@onready var water_cap_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent/InvItems/VBoxContainer/InvItemValues/VBoxContainer3/PanelContainer/HBoxContainer/WaterCapValLabel
+@onready var crop_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/Inventory/Background/VBoxContainer/InvWindowContent/InvItems/VBoxContainer/InvItemValues/VBoxContainer4/PanelContainer/CropValLabel
+# Tool Display Vars
+@onready var current_tool_value = $GameWindow/Background/GameWindowContentStack/ScrollContainer/GameContentStack/ToolsAndUpgrades/Background/VBoxContainer/TnUWindowContent/ToolButtons/VBoxContainer/HBoxContainer/MarginContainer4/VBoxContainer/PanelContainer2/CurrentToolValue
+
+
+# ------- Primary Function Group ------- #
 
 func _ready() -> void:
 	game_timer.wait_time = 0.01
-	pass # Replace with function body.
-
+	update_inventory()
+	
 func _process(delta: float) -> void:
 	game_timer_display.text = format_time(elapsed_time)
 	
+
+# ------- Game Startup Funtions Group ------- #
 
 func _on_start_button_pressed() -> void:
 	print(vs.player_name)
@@ -57,14 +73,128 @@ func format_time(seconds: float) -> String:
 	
 	return hours_str + ":" + minutes_str + ":" + secs_str + "." + millis_str
 
+func update_inventory() -> void:
+	coin_value.text = str(vs.coins)
+	seed_value.text = str(vs.seeds)
+	water_value.text = str(vs.water)
+	water_cap_value.text = str(vs.watercap)
+	crop_value.text = str(vs.crops)
+
+# ------- Tool Function Group -------- #
+
+func update_tools() -> void:
+	current_tool_value.text = str(vs.current_tool)
+
+func _on_plow_button_pressed() -> void:
+	vs.current_tool = vs.plow_tool
+	update_tools()
+
+func _on_water_button_pressed() -> void:
+	vs.current_tool = vs.water_tool
+	update_tools()
+
+func _on_scythe_button_pressed() -> void:
+	vs.current_tool = vs.scythe_tool
+	update_tools()
+
+func _on_mk_1_check_button_toggled(toggled_on: bool) -> void:
+	vs.mk1_ison = toggled_on
+	print(vs.mk1_ison)
+
+func _on_mk_2_check_button_toggled(toggled_on: bool) -> void:
+	vs.mk2_ison = toggled_on
+	print(vs.mk2_ison)
+
+func _on_mk_3_check_button_toggled(toggled_on: bool) -> void:
+	vs.mk3_ison = toggled_on
+	print(vs.mk3_ison)
+
+# ------- Store Function Group ------- #
+
+func transaction_success_response() -> void:
+	print("Joy! :]")
+
+func transaction_fail_response() -> void:
+	print("no joy :[")
+
+# Seed Purchasing
+func _on_1x_seed_button_pressed() -> void:
+	if vs.coins >= vs.seed_price:
+		vs.coins -= vs.seed_price
+		vs.seeds += vs.seed_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+
+func _on_3x_seed_button_pressed() -> void:
+	var bulk_seed_price = vs.seed_price * 3
+	var bulk_seed_quant = vs.seed_quant * 3
+	if vs.coins >= bulk_seed_price:
+		vs.coins -= bulk_seed_price
+		vs.seeds += bulk_seed_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+		
+func _on_9x_seed_button_pressed() -> void:
+	var bulk_seed_price = vs.seed_price * 9
+	var bulk_seed_quant = vs.seed_quant * 9
+	if vs.coins >= bulk_seed_price:
+		vs.coins -= bulk_seed_price
+		vs.seeds += bulk_seed_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+
+# Water Purchasing
+func _on_10x_water_button_pressed() -> void:
+	if vs.coins >= vs.water_price:
+		vs.coins -= vs.water_price
+		vs.water += vs.water_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+
+func _on_30x_water_button_pressed() -> void:
+	# ADD WATER CAP CHECK
+	var bulk_water_price = vs.water_price * 3
+	var bulk_water_quant = vs.water_quant * 3
+	if vs.coins >= bulk_water_price:
+		vs.coins -= bulk_water_price
+		vs.water += bulk_water_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+
+func _on_90x_water_button_pressed() -> void:
+	# ADD WATER CAP CHECK
+	var bulk_water_price = vs.water_price * 9
+	var bulk_water_quant = vs.water_quant * 9
+	if vs.coins >= bulk_water_price:
+		vs.coins -= bulk_water_price
+		vs.water += bulk_water_quant
+		update_inventory()
+		transaction_success_response()
+	else:
+		transaction_fail_response()
+		
+
+# -------- UI Function Stack ------- #
 
 # Game Content Stack Window Minimize Functions
-
 func _on_inv_min_button_pressed() -> void:
 	inv_body.visible = not inv_body.visible
 
 func _on_tn_u_min_button_pressed() -> void:
-	tnu_body
+	tnu_body.visible = not tnu_body.visible
 
 func _on_str_min_button_pressed() -> void:
-	
+	str_body.visible = not str_body.visible
+
+func _on_fld_min_button_pressed() -> void:
+	fld_body.visible = not fld_body.visible
--- alpha01/variable_storage.gd
+++ alpha01/variable_storage.gd
@@ -2,4 +2,35 @@ extends Node
 
 var player_name: String = "Bean"
 
-var coins: int = 1
+# Primary Inventory Variables
+var coins: int = 10
+var seeds: int = 1
+var crops: int = 1
+var water: int = 10
+var watercap: int = 10 # limits the total amount of water the player can have.
+
+# Tool Variables
+var current_tool: String
+var plow_tool: String = "Plow"
+var water_tool: String = "Watering Can"
+var scythe_tool: String = "Scythe"
+
+var mk1_ison: bool = false
+var mk2_ison: bool = false
+var mk3_ison: bool = false
+
+# Store Variables
+# Prices
+var seed_price: int = 1
+var water_price: int = 1
+var crop_price: int = 1
+var plot_price: int = 10
+#Quantities
+var seed_quant: int = 1
+var water_quant: int = 10
+var crop_quant: int = 1
+
+# Upgrade Unlock Token Bool Variables
+var mk1_isunlocked: bool = false
+var mk2_isunlocked: bool = false
+var mk3_isunlocked: bool = false
